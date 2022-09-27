a0a570a8571d6fbecc38970ee95f6caf65cf3576
LZL <106784487+lzl199704@users.noreply.github.com>
2022-09-27 13:37:00 -0400

    Update ILD_joint.py

--- code/ILD_joint.py
+++ code/ILD_joint.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import backend
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import InceptionResNetV2
@@ -12,12 +11,10 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
 from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint,Callback, TensorBoard
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import regularizers, activations
 import os
-from time import time
 from tensorflow.keras.applications.imagenet_utils import preprocess_input
 from sklearn.metrics import roc_auc_score
-import matplotlib.pyplot as plt
+
 import argparse
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras import Input
