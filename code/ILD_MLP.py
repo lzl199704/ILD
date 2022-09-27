aee282ef178adbd74d9037912023c7643e02456c
LZL <106784487+lzl199704@users.noreply.github.com>
2022-09-27 12:07:17 -0400

    Update ILD_MLP.py

--- code/ILD_MLP.py
+++ code/ILD_MLP.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from time import time
 from sklearn.metrics import roc_auc_score
-import cv2
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
@@ -18,15 +17,11 @@ args = parser.parse_args()
 df_t=pd.read_csv(args.train_path)
 df_v=pd.read_csv(args.val_path)
 df_test = pd.read_csv(args.test_path)
-
-
 df_test.MRN=df_test.MRN.astype(str)
 df_t.MRN=df_t.MRN.astype(str)
 df_test=df_test.reset_index(drop=True)
 
-
 ss = StandardScaler()
-
 
 df2_train=df_t.drop_duplicates(subset='MRN',keep='first')
 df2_val=df_v.drop_duplicates(subset='MRN',keep='first')
@@ -36,20 +31,16 @@ df2_val=df2_val.reset_index(drop=True)
 df2_test=df2_test.reset_index(drop=True)
 
 
-
-x_train=df2_train[[
+train_variables = [
        'kvp','manufacturer','Home_O2','Occupation','CurrentSmoker','FormerSmoker','Hx_disease','sex','age',
-          'pulm_HTN','Biopsy','FEV1','FVC','FEV1/FVC','DLCO','thickness']]
+          'pulm_HTN','Biopsy','FEV1','FVC','FEV1/FVC','DLCO','thickness']
+x_train=df2_train[train_variables]
 y_train=df2_train['Consensus']
 
-x_val=df2_val[[
-       'kvp','manufacturer','Home_O2','Occupation','CurrentSmoker','FormerSmoker','Hx_disease','sex','age',
-          'pulm_HTN','Biopsy','FEV1','FVC','FEV1/FVC','DLCO','thickness']]
+x_val=df2_val[train_variables]
 y_val=df2_val['Consensus']
 
-x_test=df2_test[[
-       'kvp','manufacturer','Home_O2','Occupation','CurrentSmoker','FormerSmoker','Hx_disease','sex','age',
-          'pulm_HTN','Biopsy','FEV1','FVC','FEV1/FVC','DLCO','thickness']]
+x_test=df2_test[train_variables]
 y_test=df2_test['Consensus']
 
 
@@ -59,17 +50,17 @@ x_train = pd.DataFrame(ss.fit_transform(x_train),columns = x_train.columns)
 x_val = pd.DataFrame(ss.fit_transform(x_val),columns = x_val.columns)
 
 n_classes=5
-y1_mlp=y_train
-y1_mlp=label_binarize(y1_mlp, classes=[0, 1, 2, 3,4])
+ytrain_mlp=y_train
+ytrain_mlp=label_binarize(y1_mlp, classes=[0, 1, 2, 3,4])
 
-y2_mlp=y_val
-y2_mlp=label_binarize(y2_mlp, classes=[0, 1, 2, 3,4])
+yval_mlp=y_val
+yval_mlp=label_binarize(y2_mlp, classes=[0, 1, 2, 3,4])
 
-y3_mlp=y_test
-y3_mlp=label_binarize(y3_mlp, classes=[0, 1, 2, 3,4])
+ytest_mlp=y_test
+ytest_mlp=label_binarize(y3_mlp, classes=[0, 1, 2, 3,4])
 
 
-
+###MLP classifier to predict ILD subtypes using train_variables
 random_state = np.random.RandomState(0)
 model = MLPClassifier(solver='adam', alpha=0.001, learning_rate='constant', activation='relu',
                    hidden_layer_sizes=(32,32), random_state=1)
@@ -83,21 +74,6 @@ fpr_mlp = dict()
 tpr_mlp = dict()
 roc_auc_mlp = dict()
 for i in range(n_classes):
-    fpr_mlp[i], tpr_mlp[i], _ = roc_curve(y3_mlp[:, i], y_score[:, i])
+    fpr_mlp[i], tpr_mlp[i], _ = roc_curve(ytest_mlp[:, i], y_score[:, i])
     roc_auc_mlp[i] = auc(fpr_mlp[i], tpr_mlp[i])
     print(roc_auc_mlp[i])
-
-df_prob1 = pd.DataFrame({'MRN':df2_test.MRN,'filename':df2_test.filename, 'label':y_test})
-d_pred2 = pd.DataFrame(y_score)
-d_pred2.columns = ["chp","nsip","o","sar",'uip']
-df_prob1 = pd.concat([df_prob1, d_pred2], axis=1)
-
-
-def get_series_name(f):
-    x = f[:-8]
-    return x
-
-d1=y_score
-s1=np.argmax(d1,axis=1)
-tru_label=y_val.to_numpy()
-accuracy_score(tru_label,s1)
