e45520591b16969992529c85ee9534269eea3c12
LZL <106784487+lzl199704@users.noreply.github.com>
2022-09-27 13:35:02 -0400

    Update ILD_CT_CNN.py

--- code/ILD_CT_CNN.py
+++ code/ILD_CT_CNN.py
@@ -2,23 +2,18 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import backend
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import InceptionResNetV2
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, GlobalAveragePooling2D, BatchNormalization
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation, Concatenate, Lambda
 from tensorflow.keras.models import Model
 from tensorflow.keras import layers
 from tensorflow.keras.losses import BinaryCrossentropy, CategoricalCrossentropy
-from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint,Callback, TensorBoard
+from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint,Callback
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras import regularizers, activations
 import os
-from time import time
 from tensorflow.keras.applications.imagenet_utils import preprocess_input
 from sklearn.metrics import roc_auc_score
-import cv2
 
 import argparse
 parser = argparse.ArgumentParser()
